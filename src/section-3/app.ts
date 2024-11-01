9de11f31c503acbafcdf6b9071679e871e1f272b
Tadeu17 <17.tadeu.1989@gmail.com>
2024-11-01 00:53:00 +0000

    finished class 7, partial work here, notes on notion, update github

--- src/section-3/app.ts
+++ src/section-3/app.ts
@@ -1,7 +1,7 @@
 let appId = 'abc';
 const button = document.querySelector('button')!;
 
-function add(n1: number, n2: number) {
+function Sec2add(n1: number, n2: number) {
   if (n1 + n2 > 0) {
     return n1 + n2;
   }
